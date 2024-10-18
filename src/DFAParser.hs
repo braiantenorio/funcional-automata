e1a5dfb3d4d9932f11848d090453d20f81def73d
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-10-18 13:36:12 -0300

    test

--- src/DFAParser.hs
+++ src/DFAParser.hs
@@ -23,6 +23,25 @@ parseExec s = exec
   where
     (_, _, exec) = parts s
     
+example = "player1 player2 dealer end\n\
+           \end\n\
+           \player1 Hit player1\n\
+           \player1 Stand player2\n\
+           \player2 Hit player2\n\
+           \player2 Stand dealer\n\
+           \dealer Hit dealer\n\
+           \dealer Stand end\n\
+           \end Hit end\n\
+           \end Stand end\n\
+           \#\n\
+           \Stand\n\
+           \Hit\n\
+           \Hit\n\
+           \Stand\n\
+           \Hit\n\
+           \Stand"
+
+
 {-
 parseDFA :: String -> DFA String
 parseDFA s = DFA qs delta accept
