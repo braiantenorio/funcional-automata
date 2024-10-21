1943ff5453e9c3d8168a3cb433d61f55ed97f7fa
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-10-21 15:53:39 -0300

    fromdeltastofnc

--- src/DFA.hs
+++ src/DFA.hs
@@ -1,19 +1,17 @@
 module DFA where
 
 import Data.List
+import Data.Maybe
 
 data DFA = DFA {
     qs      :: [String],
     accept  :: [String]
 }
-
-deltaFromTriples :: [(s, c, s)] -> s -> c -> s
-deltaFromTriples triples estado_actual coso =  fff (find (== (estado_actual,coso,_)) triples)
-    where 
-        fff r
-        | Just _ = 
-        | Maybe _ = 
-
+deltaFromTriples :: (Eq c, Eq s) => [(s, c, s)] -> s -> c -> s
+deltaFromTriples triples estado_actual coso =
+  case find (\(inicial, c', final) -> inicial == estado_actual && c' == coso) triples of
+    Just (_, _, toState) -> toState
+    Nothing -> estado_actual
 --fromMaybe (-1) (find (==4) [1,2,3,4])
 
 --evalDFA:: (s -> c -> s) -> DFA s -> [c] -> [s]
